8b7e604bc34ff0705334d7b35a9de284796cca3c
Rafael Encinas <rafaeladalbertoencinas94@gmail.com>
2023-03-03 13:59:19 -0700

    Se separaron las pruebas en varios tests() para mostrar individualmente cada prueba en la consola

--- tests/fizzBuzz.test.js
+++ tests/fizzBuzz.test.js
@@ -1,10 +1,31 @@
 const { default: expect } = require('expect');
 const {fizzBuzzer, fizzBuzzerCase} = require('../fizzBuzz');
 
-test('El clasico FizzBuzz.\nDivisible de 3 -> Fizz\nDivisible de 5-> Buzz\nDivisble entre los dos -> FizzBuzz\nNo es divisible entre ninguno -> Parametro',()=>{
+// Fizz 
+test('El clasico FizzBuzz. Divisible de 3 -> Fizz',()=>{
     expect(fizzBuzzer(3)).toBe('Fizz');
+    expect(fizzBuzzer(6)).toBe('Fizz');
+    expect(fizzBuzzer(9)).toBe('Fizz');
+});
+
+// Buzz
+test('El clasico FizzBuzz. Divisible de 5-> Buzz',()=>{
     expect(fizzBuzzer(5)).toBe('Buzz');
+    expect(fizzBuzzer(10)).toBe('Buzz');
+});
+
+// FizzBuzz
+test('El clasico FizzBuzz. Divisble entre los dos -> FizzBuzz',()=>{
     expect(fizzBuzzer(15)).toBe('FizzBuzz');
+    expect(fizzBuzzer(30)).toBe('FizzBuzz');
+});
+
+// Numero no divisible -> num
+test('El clasico FizzBuzz. No es divisible entre ninguno -> Parametro de entrada',()=>{
     expect(fizzBuzzer(7)).toBe(7);
-})
+    expect(fizzBuzzer(13)).toBe(13);
+    expect(fizzBuzzer(17)).toBe(17);
+});
+
+
 
